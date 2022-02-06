b8de19b1f9ab156e0982afd44b6d75273f98a310
Bockey <71261529+Bockey@users.noreply.github.com>
2022-02-06 15:42:09 +0100

    Inheritance in typescript

--- index.ts
+++ index.ts
@@ -60,3 +60,175 @@ class User{
 const user = new User("Michael", "Jordan", "jordan@nba.com");
 console.log(user.fullName);
 console.log(user.isMatching("jordan@nba.com"));
+
+
+//Inheritance in Typescript
+
+// Base/Parent Class
+
+class Animal{
+    age: number;
+    legs: number;
+    name: string;
+
+    constructor(age: number, legs: number, name: string){
+        this.age = age;
+        this.legs = legs;
+        this.name = name;
+    }
+}
+
+// const dog = new Animal(7, 4, "Beba");
+
+// console.log("My dog has " + dog.legs + " legs." + " She is " + dog.age + " years old, " + "and her name is " + dog.name + "!");
+
+// Derived/Child Class
+
+class Dog extends Animal{
+    woof(): string{
+        return "WOOF! WOOF! WOOF!"
+    }
+}
+
+class Cat extends Animal{
+    meow(): string{
+        return "MEOW! HISS! HISS!"
+    }
+}
+
+const maca = new Cat(19, 4, "Maca");
+const kuca = new Dog(7, 4, "Kuca");
+
+console.log("Name: " + kuca.name + ", Age: " + kuca.age + ", Legs nr: " + kuca.legs + ", Sound: " + kuca.woof());
+console.log("Name: " + maca.name + ", Age: " + maca.age + ", Legs nr: " + maca.legs + ", Sound: " + maca.meow());
+
+
+//Implements - Needs same shape
+//Redefine properties and methods (if everythin is not in the same 
+// file we wouldn't be able to see those using extends)
+//Even it has same shape it isn't child of Animal (extends is)
+
+class Puppy implements Animal{
+    age: number;
+    legs: number;
+    name: string;
+
+    woof(): string{
+        return "WOOF! WOOF! WOOF!";
+    }
+}
+
+//Super()
+//With extend if we use constructor we need to use super() also
+
+
+class Snake extends Animal{
+    constructor(data: {age: number; legs: number; name: string;}) {
+
+        super(data.age, data.legs, data.name);
+        
+    }
+    ssss(): string{
+        return "SSSS! SSS! SSSS!"
+    }
+}
+
+// Extends and super() overrides properties and methods from parent class
+
+//Generics
+
+class Username<T>{
+    name: string;
+    age: number;
+    email: string;
+    isMale: boolean;
+    public classicUserData: T;
+
+    public mergeClassicUser(params: T): void{
+        const { name, isMale, age, email} = this;
+
+        this.classicUserData = { name, isMale, age, email, ...params}
+    }
+}
+
+interface ClassicUser{
+    name: { first: string, last: string};
+}
+
+interface ClassicUser2 {
+    name: {first: string, middle: string, last: string}
+}
+
+const user1 = new Username<ClassicUser>();
+const user2 = new Username<ClassicUser2>();
+user1.mergeClassicUser({name: {first: "Dylan", last: "Israel"}});
+user2.mergeClassicUser({name: {first: "Dylan", last: "Israel", middle: "Christopher"}});
+console.log(user1);
+console.log(user2);
+user1.classicUserData.name.first;
+user2.classicUserData.name.middle;
+
+//Polymorphism
+//is 3 things
+/*
+1.method / param overriding -> child overrides parent
+2.method overloading -> same name methods (JS doesn't support this)
+3.interfaces / abstract classes implements
+*/
+
+// challenge2: Inheritence
+/*
+1. Use the FreshUser class below.
+2. Create an Admin class that extends FreshUser.
+3. Create a Guest class that implements FreshUser.
+4. Have both classes below pass firstName, lastName and email in their constructors
+ */
+
+
+class FreshUser {
+    firstName: string;
+    lastName: string;
+    email: string;
+
+    get fullName(): string{
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    doesEmailMatch(email: string): boolean{
+        return this.email === email;
+    }
+}
+
+class Admin extends FreshUser{
+    constructor(firstName: string, lastName: string, email: string){
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    get fullName(): string {
+        return `${this.firstName} - Admin`
+    }
+}
+class Guest implements FreshUser{
+    firstName: string;
+    lastName: string;
+    email: string;
+
+    constructor(firstName: string, lastName: string, email: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    get fullName(): string{
+        return `${this.firstName} - Guest User`;
+    }
+    doesEmailMatch(email: string): boolean{
+        return this.email === email;
+    }
+}
+
+const guestUser = new Guest("Steph", "Curry", "steph@3.com");
+const adminUser = new Admin("Charles", "Barkley", "charlie@nba.com");
+console.log(adminUser, adminUser.fullName);
+console.log(guestUser, guestUser.fullName);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var Message = /** @class */ (function () {
     function Message(title, message) {
         this.title = title;
@@ -59,3 +85,152 @@ var User = /** @class */ (function () {
 var user = new User("Michael", "Jordan", "jordan@nba.com");
 console.log(user.fullName);
 console.log(user.isMatching("jordan@nba.com"));
+//Inheritance in Typescript
+// Base/Parent Class
+var Animal = /** @class */ (function () {
+    function Animal(age, legs, name) {
+        this.age = age;
+        this.legs = legs;
+        this.name = name;
+    }
+    return Animal;
+}());
+// const dog = new Animal(7, 4, "Beba");
+// console.log("My dog has " + dog.legs + " legs." + " She is " + dog.age + " years old, " + "and her name is " + dog.name + "!");
+// Derived/Child Class
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog.prototype.woof = function () {
+        return "WOOF! WOOF! WOOF!";
+    };
+    return Dog;
+}(Animal));
+var Cat = /** @class */ (function (_super) {
+    __extends(Cat, _super);
+    function Cat() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Cat.prototype.meow = function () {
+        return "MEOW! HISS! HISS!";
+    };
+    return Cat;
+}(Animal));
+var maca = new Cat(19, 4, "Maca");
+var kuca = new Dog(7, 4, "Kuca");
+console.log("Name: " + kuca.name + ", Age: " + kuca.age + ", Legs nr: " + kuca.legs + ", Sound: " + kuca.woof());
+console.log("Name: " + maca.name + ", Age: " + maca.age + ", Legs nr: " + maca.legs + ", Sound: " + maca.meow());
+//Implements - Needs same shape
+//Redefine properties and methods (if everythin is not in the same 
+// file we wouldn't be able to see those using extends)
+//Even it has same shape it isn't child of Animal (extends is)
+var Puppy = /** @class */ (function () {
+    function Puppy() {
+    }
+    Puppy.prototype.woof = function () {
+        return "WOOF! WOOF! WOOF!";
+    };
+    return Puppy;
+}());
+//Super()
+//With extend if we use constructor we need to use super() also
+var Snake = /** @class */ (function (_super) {
+    __extends(Snake, _super);
+    function Snake(data) {
+        return _super.call(this, data.age, data.legs, data.name) || this;
+    }
+    Snake.prototype.ssss = function () {
+        return "SSSS! SSS! SSSS!";
+    };
+    return Snake;
+}(Animal));
+// Extends and super() overrides properties and methods from parent class
+//Generics
+var Username = /** @class */ (function () {
+    function Username() {
+    }
+    Username.prototype.mergeClassicUser = function (params) {
+        var _a = this, name = _a.name, isMale = _a.isMale, age = _a.age, email = _a.email;
+        this.classicUserData = __assign({ name: name, isMale: isMale, age: age, email: email }, params);
+    };
+    return Username;
+}());
+var user1 = new Username();
+var user2 = new Username();
+user1.mergeClassicUser({ name: { first: "Dylan", last: "Israel" } });
+user2.mergeClassicUser({ name: { first: "Dylan", last: "Israel", middle: "Christopher" } });
+console.log(user1);
+console.log(user2);
+user1.classicUserData.name.first;
+user2.classicUserData.name.middle;
+//Polymorphism
+//is 3 things
+/*
+1.method / param overriding -> child overrides parent
+2.method overloading -> same name methods (JS doesn't support this)
+3.interfaces / abstract classes implements
+*/
+// challenge2: Inheritence
+/*
+1. Use the FreshUser class below.
+2. Create an Admin class that extends FreshUser.
+3. Create a Guest class that implements FreshUser.
+4. Have both classes below pass firstName, lastName and email in their constructors
+ */
+var FreshUser = /** @class */ (function () {
+    function FreshUser() {
+    }
+    Object.defineProperty(FreshUser.prototype, "fullName", {
+        get: function () {
+            return "".concat(this.firstName, " ").concat(this.lastName);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    FreshUser.prototype.doesEmailMatch = function (email) {
+        return this.email === email;
+    };
+    return FreshUser;
+}());
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin(firstName, lastName, email) {
+        var _this = _super.call(this) || this;
+        _this.firstName = firstName;
+        _this.lastName = lastName;
+        _this.email = email;
+        return _this;
+    }
+    Object.defineProperty(Admin.prototype, "fullName", {
+        get: function () {
+            return "".concat(this.firstName, " - Admin");
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Admin;
+}(FreshUser));
+var Guest = /** @class */ (function () {
+    function Guest(firstName, lastName, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    Object.defineProperty(Guest.prototype, "fullName", {
+        get: function () {
+            return "".concat(this.firstName, " - Guest User");
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Guest.prototype.doesEmailMatch = function (email) {
+        return this.email === email;
+    };
+    return Guest;
+}());
+var guestUser = new Guest("Steph", "Curry", "steph@3.com");
+var adminUser = new Admin("Charles", "Barkley", "charlie@nba.com");
+console.log(adminUser, adminUser.fullName);
+console.log(guestUser, guestUser.fullName);
